a4d02a893efd75c42a84d3ddb5789f7aa866dd27
Naveen selvam <achanandhi.m@gmail.com>
2022-10-20 20:14:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import CountClass from "./CountClass";
+import { useState } from "react";
+import "./App.css";
+import MainRouter from "./Router/Router";
 
 function App() {
-  const [count,setCount] = useState(0)
-  const clickable = () => {
-    setCount(count + 1)
-  }
+
   return (
     <div>
-      <div style={{ textAlign:"center" }}>Fun Count :{count} </div>
-      <button onClick={() => clickable()}>Click Me !</button>
-      </div>
+      <MainRouter />
+    </div>
   );
 }
 
